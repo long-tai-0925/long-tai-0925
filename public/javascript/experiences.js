43bd2617df1994d34202072229d525abb90fd364
Jiang <me@longtai.org>
2026-04-30 16:54:50 +0800

    add years tag

--- public/javascript/experiences.js
+++ public/javascript/experiences.js
@@ -6,6 +6,7 @@ export const experienceConfig = {
       <div class="card">
         <img loading="lazy" src="${i.img}" alt="${i.alt}">
         <p>
+          ${i.years ? `<span class="years">${i.years}</span><br/>` : ''}
           ${i.title}<br/>
           ${i.subtitle ? i.subtitle + '<br/>' : ''}
           ${i.role}
